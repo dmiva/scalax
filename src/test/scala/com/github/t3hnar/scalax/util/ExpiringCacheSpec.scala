9d9b40976ab07ac9d1e02b3d269144282b39d3dd
Dmitrijs Ivančenko <dmitry1991@gmail.com>
2020-10-25 23:14:06 +0200

    Added test case for ExpiringCache

--- src/test/scala/com/github/t3hnar/scalax/util/ExpiringCacheSpec.scala
+++ src/test/scala/com/github/t3hnar/scalax/util/ExpiringCacheSpec.scala
@@ -44,6 +44,17 @@ class ExpiringCacheSpec extends Specification {
       cache.map.size must eventually(beEqualTo(1))
     }
 
+    "not contain value after it is explicitly removed" in new ExpiringCacheScope {
+      cache.map must haveSize(0)
+
+      cache.put(0, "0")
+      cache.map must haveSize(1)
+
+      cache.remove(0)
+      cache.get(0) must beNone
+      cache.map must haveSize(0)
+    }
+
   }
 
   class ExpiringCacheScope extends Scope {
